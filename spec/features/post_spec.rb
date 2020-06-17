9738f3a7915326ad91e36591d2d40a31f6ec3902
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-06-17 00:45:15 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,8 +4,7 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
-  it 'renders HTML in the /new template' do
-    visit new_post_path
-    expect(page).to have_content('Post Form')
+    
   end
+  
 end
